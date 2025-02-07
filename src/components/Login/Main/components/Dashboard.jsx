de4af20664d7903e47f8d5d3b4538048d296a6bf
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2025-02-07 11:41:07 +0530

    dashboard

--- src/components/Login/Main/components/Dashboard.jsx
+++ src/components/Login/Main/components/Dashboard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/AdminHome.css";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="Dashboard-parent">
       <h1 style={{ color: "white" }}>Dashboard</h1>
       <h2>This is admin Dashboard</h2>
     </div>
