083e93a17960618beede8e6549664113f4a38556
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2025-02-10 22:18:23 +0530

    Update EditBus.jsx

--- src/components/Login/Main/AdminComponents/EditBus.jsx
+++ src/components/Login/Main/AdminComponents/EditBus.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/AdminHome.css";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
